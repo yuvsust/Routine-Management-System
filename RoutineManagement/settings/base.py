6e3b7f1c66ed4392fdd52aa3b99704e2116a409b
Tanvir Hossain Yuvraz <33263219+yuvsust@users.noreply.github.com>
2023-07-08 22:16:30 +0600

    Update base.py

--- RoutineManagement/settings/base.py
+++ RoutineManagement/settings/base.py
@@ -119,5 +119,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tanvirhossainyuvraz@gmail.com'
-EMAIL_HOST_PASSWORD = 't0m@rv@ng@smr!t!'
+# Email host user and password is here
